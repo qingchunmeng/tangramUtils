2502f695c28096e21f6850bd7976b53e00c88650
孟青春 <mengqingchun002@ke.com>
2019-04-02 20:32:46 +0800

    feature-修改util路径

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Dialog from './components/dialog/js/index.js';
 import Fetch from './components/fetch/js/index.js';
 import Loading from './components/loading/js/index.js';
 import Cookie from './components/cookie/js/index.js';
-import Util from './util/debounce.js/index.js';
+import Util from './util/index.js';
 
 export {
     Fetch,
