969bb1e9e9567115fe29c7d470026354bd56cfe2
孟青春 <mengqingchun002@ke.com>
2019-04-19 10:53:16 +0800

    feature-增加新的util方法

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,19 @@
 import debounce from './debounce.js';
+import cache from './cacheUtil.js';
+import dataProcess from './dataProcess';
+import dom from './dom.js';
+import file from './fileUtil.js';
+import paramsProcess from './paramsProcess.js';
+import validate from './validateUtil.js';
 import dict from './dict.js';
 
 export default {
     debounce,
     dict,
+    cache,
+    dataProcess,
+    dom,
+    file,
+    validate,
+    paramsProcess,
 };
