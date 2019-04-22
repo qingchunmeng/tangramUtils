84335608e1c5316fb52e804d6b79d0e7679a2d9e
Gao Rui <gaorui@ke.com>
2019-04-22 17:50:00 +0800

    feature 添加函数isReturnValue

--- src/util/validate.js
+++ src/util/validate.js
@@ -5,6 +5,22 @@
  */
 
 const validate = {
+    isReturnValue: (judge, value) => {
+        let returnValue;
+        if (typeof judge === 'string' || Array.isArray(judge)) {
+            returnValue = judge.length > 0 ? value : '';
+        }
+        if (typeof judge === 'object') {
+            returnValue = JSON.stringify(judge) !== '{}' ? value : '';
+        }
+        if (typeof judge === 'number') {
+            returnValue = judge > 0 ? value : '';
+        }
+        if (typeof judge === 'boolean') {
+            returnValue = judge ? value : '';
+        }
+        return returnValue;
+    },
     isEmpty: () => {
 
     },
