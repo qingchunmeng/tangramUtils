d11629ff6c5618c6fa867656d77154a8acc27471
zhangwenjie009 <zhangwenjie009@ke.com>
2019-04-24 18:22:44 +0800

    feature: 添加完方法

--- src/util/dom.js
+++ src/util/dom.js
@@ -5,11 +5,48 @@
  */
 
 const dom = {
-    addEventListener: () => {
-
+    /**
+     * 兼容 ie 的 DOM 事件绑定函数
+     * @param {Node} target
+     * @param {String} eventType
+     * @param {Function} callback
+     * @return {Object} result - 属性方法 remove，直接调用 result.remove() 解除事件绑定
+     */
+    addEventListener: (target, eventType, callback) => {
+        if (target.addEventListener) {
+            target.addEventListener(eventType, callback, false);
+            return {
+                remove() {
+                    target.removeEventListener(eventType, callback, false);
+                },
+            };
+        }
+        if (target.attachEvent) {
+            target.attachEvent(`on${eventType}`, callback);
+            return {
+                remove() {
+                    target.detachEvent(`on${eventType}`, callback);
+                },
+            };
+        }
     },
-    contains: () => {
 
+    /**
+     * 判断容器节点是否包含指定节点
+     * @param {Node} root - 容器节点
+     * @param {Node} n - 指定节点
+     * @return {Boolean}
+     */
+    contains: (root, n) => {
+        let node = n;
+        while (node) {
+            if (node === root) {
+                return true;
+            }
+            node = node.parentNode;
+        }
+
+        return false;
     },
     on: () => {
 
--- src/util/file.js
+++ src/util/file.js
@@ -4,8 +4,62 @@
  * @date 2019/4/16 15:26
  */
 const file = {
-    downloadRename: () => {
+    /**
+    * 下载
+    * @param  {String} url 目标文件地址
+    * @param  {String} filename 想要保存的文件名称
+    */
+    downloadRename: (url, filename) => {
+        /**
+        * 获取 blob
+        * @param  {String} url 目标文件地址
+        * @return {Promise}
+        */
+        function getBlob(_url) {
+            return new Promise((resolve) => {
+                const xhr = new XMLHttpRequest();
 
+                xhr.open('GET', _url, true);
+                xhr.responseType = 'blob';
+                xhr.onload = () => {
+                    if (xhr.status === 200) {
+                        resolve(xhr.response);
+                    }
+                };
+
+                xhr.send();
+            });
+        }
+
+        /**
+        * 保存
+        * @param  {Blob} blob
+        * @param  {String} filename 想要保存的文件名称
+        */
+        function saveAs(blob, _filename) {
+            if (window.navigator.msSaveOrOpenBlob) {
+                navigator.msSaveBlob(blob, _filename);
+            } else {
+                const link = document.createElement('a');
+                const body = document.querySelector('body');
+
+                link.href = window.URL.createObjectURL(blob);
+                link.download = _filename;
+
+                // fix Firefox
+                link.style.display = 'none';
+                body.appendChild(link);
+
+                link.click();
+                body.removeChild(link);
+
+                window.URL.revokeObjectURL(link.href);
+            }
+        }
+
+        getBlob(url).then((blob) => {
+            saveAs(blob, filename);
+        });
     },
 
 };
