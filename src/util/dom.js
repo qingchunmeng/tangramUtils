acefa804f1cc506ccc393d797854f6c3e738ee5d
吴庆凡 <wuqingfan001@ke.com>
2019-05-10 18:41:28 +0800

    feature: 添加dom相关公共方法

--- src/util/dom.js
+++ src/util/dom.js
@@ -55,19 +55,72 @@ const dom = {
 
     },
     /**
-   * 返回android,ios,pc
-   */
-    scrollWidth: () => {
+     * 获取dom元素scrollWidth
+     * @param {Node} elem - dom元素
+     * @return {Number}
+     */
+    scrollWidth: (elem) => {
+        if (!elem) return false;
+        const isWindow = o => o != null && o === o.window;
 
+        if (isWindow(elem) || elem.nodeType === 9) { // 如果是窗口对象, 或者document对象
+            const bw = document.body ? document.body.scrollWidth : 0;
+            const dw = document.documentElement ? document.documentElement.scrollWidth : 0;
+            return Math.max(bw, dw);
+        }
+
+        return elem.scrollWidth;
     },
-    scrollHeight: () => {
+    /**
+     * 获取dom元素scrollHeight
+     * @param {Node} elem - dom元素
+     * @return {Number}
+     */
+    scrollHeight: (elem) => {
+        if (!elem) return false;
+        const isWindow = o => o != null && o === o.window;
 
+        if (isWindow(elem) || elem.nodeType === 9) { // 如果是窗口对象, 或者document对象
+            const bh = document.body ? document.body.scrollHeight : 0;
+            const dh = document.documentElement ? document.documentElement.scrollHeight : 0;
+            return Math.max(bh, dh);
+        }
+
+        return elem.scrollHeight;
     },
-    clientWidth: () => {
+    /**
+     * 获取dom元素clientWidth
+     * @param {Node} elem - dom元素
+     * @return {Number}
+     */
+    clientWidth: (elem) => {
+        if (!elem) return false;
+        const isWindow = o => o != null && o === o.window;
 
+        if (isWindow(elem) || elem.nodeType === 9) { // 如果是窗口对象, 或者document对象
+            const bw = document.body ? document.body.clientWidth : 0;
+            const dw = document.documentElement ? document.documentElement.clientWidth : 0;
+            return Math.max(bw, dw);
+        }
+
+        return elem.clientWidth;
     },
-    clientHeight: () => {
+    /**
+     * 获取dom元素clientHeight
+     * @param {Node} elem - dom元素
+     * @return {Number}
+     */
+    clientHeight: (elem) => {
+        if (!elem) return false;
+        const isWindow = o => o != null && o === o.window;
 
+        if (isWindow(elem) || elem.nodeType === 9) { // 如果是窗口对象, 或者document对象
+            const bh = document.body ? document.body.clientHeight : 0;
+            const dh = document.documentElement ? document.documentElement.clientHeight : 0;
+            return Math.max(bh, dh);
+        }
+
+        return elem.clientHeight;
     },
     offsetWidth: () => {
 
@@ -75,11 +128,45 @@ const dom = {
     offsetHeight: () => {
 
     },
-    windowWidth: () => {
+    /**
+     * 获取window元素宽度
+     * @param {Node} elem - dom元素
+     * @return {Number}
+     */
+    windowWidth: (elem) => {
+        if (!elem) elem = window;
+        const isWindow = o => o != null && o === o.window;
 
+        if (isWindow(elem)) { // 如果是window元素
+            return elem.document.documentElement.clientWidth;
+        }
+
+        if (elem.nodeType === 9) { // 如果是document元素
+            const doc = elem.documentElement;
+            return Math.max(elem.body.clientWidth, doc.clientWidth);
+        }
+
+        return elem.clientWidth;
     },
-    windowHeight: () => {
+    /**
+     * 获取window元素高
+     * @param {Node} elem - dom元素
+     * @return {Number}
+     */
+    windowHeight: (elem) => {
+        if (!elem) elem = window;
+        const isWindow = o => o != null && o === o.window;
 
+        if (isWindow(elem)) { // 如果是window元素
+            return elem.document.documentElement.clientHeight;
+        }
+
+        if (elem.nodeType === 9) { // 如果是document元素
+            const doc = elem.documentElement;
+            return Math.max(elem.body.clientHeight, doc.clientHeight);
+        }
+
+        return elem.clientHeight;
     },
 
 };
