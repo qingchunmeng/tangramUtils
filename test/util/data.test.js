5ef1a120b2a403014508b8c05a38b91c7e7548db
lijinpeng <lijinpeng007@ke.com>
2019-06-19 10:42:53 +0800

    feature:完成 randomNum 的单测

--- test/util/data.test.js
+++ test/util/data.test.js
@@ -7,7 +7,7 @@
 import data from '../../src/util/data';
 
 const {
-    formatReturnValue, deepClone,
+    formatReturnValue, deepClone, randomNum,
 } = data;
 
 /* eslint-disable */
@@ -56,6 +56,77 @@ describe('deepClone', () => {
                 expect(result).toEqual(orginalData);
             });
         });
+    });
+});
+
+describe('randomNum', () => {
+    test('给定两个整数 min max，返回 [min, max] 的整数', () => {
+        const min = 1;
+        const max = 5;
+        const num = randomNum(min, max);
+        expect(num >= min && num <= max && num%1 == 0).toBeTruthy();
+    });
+
+    test('给定两个数 min max，min 为小数，返回 [min, max] 的小数', () => {
+        const min = 1.2;
+        const max = 5;
+        const num = randomNum(min, max);
+        expect(num >= min && num <= max && num%1 != 0).toBeTruthy();
+    });
+
+    test('给定两个数 min max，max 为小数，返回 [min, max] 的小数', () => {
+        const min = 1;
+        const max = 5.2;
+        const num = randomNum(min, max);
+        expect(num >= min && num <= max && num%1 != 0).toBeTruthy();
+    });
+
+    test('给定两个整数 min max，floating = true，返回 [min, max] 的小数', () => {
+        const min = 1;
+        const max = 5;
+        const num = randomNum(min, max, true);
+        expect(num >= min && num <= max && num%1 != 0).toBeTruthy();
+    });
+
+    test('没有传参数时，返回 0 或 1', () => {
+        const num = randomNum();
+        expect(num == 0 || num == 1).toBeTruthy();
+    });
+
+    test('只传一个整数 number 时，返回 [0, number] 的整数', () => {
+        const num = randomNum(5);
+        expect(num >= 0 && num <= 5 && num%1 == 0).toBeTruthy();
+    });
+
+    test('只传一个整数 number ，第二个参数为 true 时，返回 [0, number] 的小数', () => {
+        const num = randomNum(5, true);
+        expect(num >= 0 && num <= 5 && num%1 != 0).toBeTruthy();
+    });
+
+    test('只传一个整数 number ，第二个参数为 false 时，返回 [0, number] 的整数', () => {
+        const num = randomNum(5, false);
+        expect(num >= 0 && num <= 5 && num%1 == 0).toBeTruthy();
+    });
+
+    test('传参数 min max，且 min > max（参数大小错位），返回 [max, min]', () => {
+        const min = 5;
+        const max = 1;
+        const num = randomNum(min, max);
+        expect(num >= max && num <= min).toBeTruthy();
+    });
+
+    test('强制类型转换，传参数为数字字符串时，强制转换为数字处理', () => {
+        const min = '1';
+        const max = '5';
+        const num = randomNum(min, max);
+        expect(num >= Number(min) && num <= Number(max)).toBeTruthy();
+    });
+
+    test('强制类型转换，传参数为非数字字符串（不能简单直接转换为数字）时，返回0', () => {
+        const min = '1a';
+        const max = '5f';
+        const num = randomNum(min, max);
+        expect(num).toBe(0);
     });
 });
 /* eslint-enable */
