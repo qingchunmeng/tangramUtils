3452add37df7de0de19780933819b46e76da2ab1
lihaixu <lihaixu@lianjia.com>
2019-09-23 20:31:39 +0800

    feature: compute 小写

--- test/util/compute.test.js
+++ test/util/compute.test.js
@@ -4,43 +4,43 @@
  * @date 2019/9/17 15:53
  */
 
-import Compute from '../../src/util/compute';
+import compute from '../../src/util/compute';
 
 describe('debounce', () => {
     test('Compute [0.1, 0.1]', () => {
-        expect(Compute.add([0.1, 0.2], 2)).toEqual(0.30);
+        expect(compute.add([0.1, 0.2], 2)).toEqual(0.30);
     });
     test('Compute [0.001, 0.001]', () => {
-        expect(Compute.add([0.001, 0.001], 2)).toEqual(0);
+        expect(compute.add([0.001, 0.001], 2)).toEqual(0);
     });
     test('Compute [0.001, 0.005]', () => {
-        expect(Compute.add([0.001, 0.005], 2)).toEqual(0.01);
+        expect(compute.add([0.001, 0.005], 2)).toEqual(0.01);
     });
     test('Compute [0.1, 0.2, 0.3], 2', () => {
-        expect(Compute.add([0.1, 0.2, 0.3], 2)).toEqual(0.60);
+        expect(compute.add([0.1, 0.2, 0.3], 2)).toEqual(0.60);
     });
     test('Compute [0.01, 0.03], 2', () => {
-        expect(Compute.add([0.01, 0.03], 2)).toEqual(0.04);
+        expect(compute.add([0.01, 0.03], 2)).toEqual(0.04);
     });
     test('Compute [0.01, 0.3], 2', () => {
-        expect(Compute.add([0.01, 0.3], 2)).toEqual(0.31);
+        expect(compute.add([0.01, 0.3], 2)).toEqual(0.31);
     });
     test('Compute [0.3, 0.1], 2', () => {
-        expect(Compute.subtract([0.3, 0.1], 2)).toEqual(0.20);
+        expect(compute.subtract([0.3, 0.1], 2)).toEqual(0.20);
     });
     test('Compute [0.2, 0.1, 0.01], 2', () => {
-        expect(Compute.subtract([0.2, 0.1, 0.01], 2)).toEqual(0.09);
+        expect(compute.subtract([0.2, 0.1, 0.01], 2)).toEqual(0.09);
     });
     test('Compute [0.008, 0.09], 2', () => {
-        expect(Compute.subtract([0.008, 0.09], 2)).toEqual(-0.08);
+        expect(compute.subtract([0.008, 0.09], 2)).toEqual(-0.08);
     });
     test('Compute [0.008, 0.009], 2', () => {
-        expect(Compute.subtract([0.008, 0.009], 2)).toEqual(-0.00);
+        expect(compute.subtract([0.008, 0.009], 2)).toEqual(-0.00);
     });
     test('Compute [0.008, 0.09], 3', () => {
-        expect(Compute.subtract([0.008, 0.09], 3)).toEqual(-0.082);
+        expect(compute.subtract([0.008, 0.09], 3)).toEqual(-0.082);
     });
     test('Compute [0.008, 0.09], 2', () => {
-        expect(Compute.subtract([0.008, 0.009], 2)).toEqual(-0.00);
+        expect(compute.subtract([0.008, 0.009], 2)).toEqual(-0.00);
     });
 });
