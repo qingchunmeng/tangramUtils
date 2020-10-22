959fc241543ff4fe9a52d330cb489ba90a029e06
mengqingchun002 <mengqingchun002@ke.com>
2020-10-22 21:53:34 +0800

    feat: 增加日期格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     ...EslintConfig,
     rules: {
         ...EslintConfig.rules,
-        eqeqeq: [1]
+        eqeqeq: [1],
+        'no-param-reassign': [1],
+        'no-restricted-syntax': [1],
+        'guard-for-in': [1]
     }
 };
