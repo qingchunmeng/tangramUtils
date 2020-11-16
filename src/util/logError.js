9679a65854f7953765249f3e2d37d321664d0a54
lihaixu <lihaixu@ke.com>
2020-11-16 15:17:29 +0800

    feat: 优化logError方法，clearTimeout、删除上报埋点

--- src/util/logError.js
+++ src/util/logError.js
@@ -5,7 +5,8 @@
  * @Description: 错误日志上报，优先上报灯塔（report_为前缀的错误），灯塔不存在时会上报至罗盘（事件Id为'20619'）
  */
 let win = {};
-if (typeof window !== 'undefined') { // 兼容服务端渲染window不存在时
+if (typeof window !== 'undefined') {
+    // 兼容服务端渲染window不存在时
     win = window;
 }
 const { location = {} } = win;
@@ -15,44 +16,18 @@ const { location = {} } = win;
  * @param {string} errorName 错误名称
  * @param {object} errorInfo 堆栈的 info 对象
  */
-export default function (error, errorName = 'report_error', errorInfo) {
-    if (!error) {
+export default function (error, errorInfo, errorName = 'report_error') {
+    if (!error || typeof error !== 'string') {
+        console.warn('params type error: first param(error) mast be a string and not null');
         return;
     }
-    setTimeout(() => {
+    const task = setTimeout(() => {
         const { href = '' } = location;
         if (win.dt && win.dt.notify) {
-            win.dt.notify(
-                `${errorName}_${String(error).substr(0, 170)}`,
-                href.substr(0, 199),
-                errorInfo
-            );
-        } else if (win.$ULOG) {
-            const ulogConfig = (win.$ULOG.getConfig && win.$ULOG.getConfig()) || {};
-            try {
-                win.$ULOG.send('37166', {
-                    pid: 'nts_pc_errorcatch',
-                    uicode: 'error_catch',
-                    action: {
-                        pid: ulogConfig.pid || '',
-                        source_url: href.substr(0, 199),
-                        error_message: error,
-                        error_name: errorName,
-                        error_info: JSON.stringify(errorInfo) // 对应罗盘维度‘错误信息’
-                    }
-                });
-            } catch (e) {
-                win.$ULOG.send('37166', {
-                    pid: 'nts_pc_errorcatch',
-                    uicode: 'error_catch',
-                    action: {
-                        pid: ulogConfig.pid || '',
-                        source_url: href.substr(0, 199),
-                        error_name: errorName,
-                        error_message: `上报错误时报错：${e.message}` // 对应罗盘维度‘错误信息’
-                    }
-                });
-            }
+            win.dt.notify(`${errorName}_${error.substr(0, 170)}`, href.substr(0, 199), errorInfo);
+        } else {
+            console.error(error);
         }
+        clearTimeout(task);
     }, 100);
 }
