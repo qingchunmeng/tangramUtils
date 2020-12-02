259cd86b206ff9daee50a6031dec529a4c830ccf
fangt11 <fangt11@ke.com>
2020-12-02 12:17:38 +0800

    feat: htmlToImg 第一个参数支持直接传入dom元素

--- src/util/html2canvasWechat.js
+++ src/util/html2canvasWechat.js
@@ -5,9 +5,23 @@
  * Desc: html2canvas的方法重新一些，为了使用1.0.0-rc.4版本，因为微信高版本下会有问题
  */
 import html2canvas from 'html2canvas';
+import { isString } from 'lodash';
 
-export const htmlToImg = (domId, options = {}) => {
-    const dom = document.getElementById(domId);
+/**
+ * [html2canvas 包装]
+ * @param  {[string, element]} domRefer   [元素的id, 或者直接传入dom元素]
+ * @param  {Object} options [同 html2canvas 的config]
+ * @return {[promise<string>]}         [截图的base64字符串]
+ */
+export const htmlToImg = (domRefer, options = {}) => {
+    let dom;
+    if (isString(domRefer)) {
+        // 元素的id
+        dom = document.getElementById(domRefer);
+    } else {
+        // 直接传入dom元素
+        dom = domRefer;
+    }
     const domHeight = dom.scrollHeight;
     let config = {
         // 设置一个默认的配置
@@ -26,7 +40,7 @@ export const htmlToImg = (domId, options = {}) => {
     return new Promise((resolve, reject) => {
         html2canvas(dom, config)
             .then(res => {
-                resolve(res.toDataURL('image/jpg'));
+                resolve(res.toDataURL('image/jpeg'));
             })
             .catch(e => {
                 reject(e);
